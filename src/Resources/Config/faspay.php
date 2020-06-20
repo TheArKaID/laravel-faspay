7e60608cf341d870b7b1633ace5c816536839777
Arifia Kasastra R <arka.progammer@gmail.com>
2020-06-20 17:35:57 +0700

    Update config/faspay.php

--- src/Resources/Config/faspay.php
+++ src/Resources/Config/faspay.php
@@ -1,16 +1,34 @@
 <?php
 
-namespace TheArKaID\LaravelFaspay;
-
-use TheArKaID\LaravelFaspay\Debit\FaspayUser;
-
-class faspay extends FaspayUser{
-    function __construct() {
-        $this->setMerchantName("Nama Merchan");
-        $this->setMerchantId("ID Merchant");
-        $this->setUserId("User");
-        $this->setPassword("Password");
-        $this->setRedirectUrl("https://faspay.co.id");
-        $this->setDev(true); // true untuk Dev Mode, false untuk Production Mode
-    }
-}
+return [
+    /**
+     * ==========================================
+     * Set Kredensial di file.env seperti berikut
+     * ==========================================
+     * 
+     * Set true saat dalam Mode Development
+     * Set false saat ke Mode Production
+     */
+    'isdev' => true,
+    /**
+     * Kredensial saat dalam Mode Development
+     */
+    'devcred' => [
+        'merchantname' => env('FP_DEV_MERCHANT_NAME', 'OR JUST SET HERE'),
+        'merchantid' => env('FP_DEV_MERCHANT_ID', 'OR JUST SET HERE'),
+        'userid' => env('FP_DEV_USER_ID', 'OR JUST SET HERE'),
+        'password' => env('FP_DEV_PASSWORD', 'OR JUST SET HERE'),
+        'redirecturl' => env('FP_DEV_REDIRECT_URL', 'OR JUST SET HEREE')
+    ],
+    /**
+     * Kredensial saat dalam Mode Production
+     */
+    'prodcred' => [
+        'merchantname' => env('FP_PROD_MERCHANT_NAME', 'OR JUST SET HERE'),
+        'merchantid' => env('FP_PROD_MERCHANT_ID', 'OR JUST SET HERE'),
+        'userid' => env('FP_PROD_USER_ID', 'OR JUST SET HERE'),
+        'password' => env('FP_PROD_PASSWORD', 'OR JUST SET HERE'),
+        'redirecturl' => env('FP_PROD_REDIRECT_URL', 'OR JUST SET HEREE')
+    ]
+];
+?>
